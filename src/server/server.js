639a0fdf14a5db8d446f100a32fdb10b41a3290a
Nuno Neves <youknownuno@gmail.com>
2018-06-03 22:45:35 +0100

    added barebones socket api files

---
+++ src/server/server.js
@@ -0,0 +1,29 @@
+const io = require('socket.io')();
+const socketAPI = require('./socketAPI.js').socketAPI;
+
+const port = process.env.port || 8000;
+
+server = {};
+
+// setup server cache
+
+io.on('connection', (socket) => {
+
+	// setup connect
+
+	// setup getHistory
+
+	// setup sendMessage
+
+	// setup sendWriting
+
+	// setup getWriting
+
+	// setup disconnect
+
+};
+
+
+
+io.listen(port);
+console.log('listening on port ', port);
