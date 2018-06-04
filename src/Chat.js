5879257c76d00b47095409acb99493888a887f84
Nuno Neves <youknownuno@gmail.com>
2018-06-04 23:36:37 +0100

    hard coded state, replaced History component with a mapped state.history

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import ChatBox from './ChatBox.js';
-import History from './History.js';
+import ChatBubble from './ChatBubble.js';
 import WritingContainer from './WritingContainer.js';
 
 class Chat extends Component {
@@ -9,31 +9,66 @@ class Chat extends Component {
 		super(props);
 
 		this.state = {
-			username: "me",
-			history: []
+			userName: "self",
+			contactName: "other",
+			history: [
+          { 
+            author: "other",
+            content: "How are you?"
+          },
+          {
+            author: "self",
+            content: "All good in the hood. You?"
+          },
+          {
+            author: "other",
+            content: "Peachy!"
+          }
+        ]
 		}
 	}
-	// consider holding state in the parent app component
-	// displays contact name
-	// return button
-	// chat history
 
 	updateContactHistory = (contact, history) => {
 		this.props.updateContactHistory(contact, history);
 	}
+
 	// chat input box
 	goToPage = () => { this.props.goToPage("Contacts") }
 
+	componentDidMount() {
+		console.log("call socketAPI here for chat history");
+		// setState of App via props.updateContactHistory(contact, history)
+
+		// choose
+			// trigger update on chat from App
+			// or trigger it via local state
+	}
+
 	render() {
+
+		const history = this.state.history.map( (message) => {
+
+			if(message.author === this.state.userName) {
+				return <div className="other bubble">{message.content}</div>
+			}
+
+			return <div className="self bubble">{message.content}</div>
+
+		});
+
 		return (
 			<div className="page chat">
 				<div className="chatContact">
-					<div className="arrow" onClick={this.goToPage}>←</div>
-					contact name
+					<div className="arrow" onClick={this.goToPage}>
+					←
+					</div>
+					{this.state.contactName}
 				</div>
 				<div className="flexEnd">
 					<div className="scroll">
-						<History/>
+						<div className="history">
+							{history}
+						</div>
 						<WritingContainer/>
 					</div>
 					<ChatBox/>
