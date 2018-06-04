266ac99ae000d0e84a5f6bec330fc84c39e11532
Nuno Neves <youknownuno@gmail.com>
2018-06-04 23:39:42 +0100

    replaced Contact component with mapped version of props.contacts array

--- src/Contacts.js
+++ src/Contacts.js
@@ -5,9 +5,18 @@ const Contacts = (props) => {
 
 	const goToPage = () => { props.goToPage("AddContact") }
 
+	const contacts = props.contacts.map( (contact) => {
+		return (
+			<Contact
+				goToPage={props.goToPage}
+				username={contact}
+			/>
+		);
+	});
+
 	return (
 		<div className="page contacts">
-			<Contact goToPage={props.goToPage}/>
+			{contacts}
 			<div className="bubble" onClick={goToPage}>add</div>
 		</div>
 	)
