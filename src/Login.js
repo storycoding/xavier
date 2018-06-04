2acc13f62d78b90bfc011bb49feb145d11f3f49d
Nuno Neves <youknownuno@gmail.com>
2018-06-04 23:41:06 +0100

    added state with username and password, added submit stub

--- src/Login.js
+++ src/Login.js
@@ -3,22 +3,30 @@ import React, { Component } from 'react';
 class Login extends Component {
 	constructor(props) {
 		super(props);
-		// controlled component
-		// holds state for API calls to the socket
-			// email
-			// password
+		this.state = {
+			username: "me",
+			password: "password"
+		}
 	}
-	
+
 	goToPage = () => {
 		this.props.goToPage("Contacts");
 	}
+
+	submit = () => {
+		//make api call to socket
+			// use email and password
+		this.goToPage();
+	}
+	
+	
 
 	render() {
 		return (
 			<div className="page login">
 				<input type="text" placeholder="email" className="bubble"></input>
 				<input type="text" placeholder="password" className="bubble"></input>
-				<div className="bubble" onClick={this.goToPage}>login</div>
+				<div className="bubble" onClick={this.submit}>login</div>
 			</div>
 			);
 	}
