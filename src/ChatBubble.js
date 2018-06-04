68d7553686330149037d5d6171cf6fc5b002af9f
Nuno Neves <youknownuno@gmail.com>
2018-06-04 23:57:38 +0100

    replaced Contact with mapped version of props.contact array

--- src/ChatBubble.js
+++ src/ChatBubble.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const ChatBubble = (props) => (
-	<div className="bubble">hello</div>
-);
+const ChatBubble = (props) => {
+	if(props.author === "self" ) {
+		return <div className="self bubble">{props.content}</div>
+	}
+
+	return <div className="other bubble">{props.content}</div>
+}
 
 export default ChatBubble;
--- src/Contacts.js
+++ src/Contacts.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import Contact from './Contact.js';
 
 const Contacts = (props) => {
 
-	const goToPage = () => { props.goToPage("AddContact") }
+	const goToAddContact = () => { props.goToPage("AddContact") }
 
+	const goToChat = () => { props.goToPage("Chat") }
 	const contacts = props.contacts.map( (contact) => {
 		return (
-			<Contact
-				goToPage={props.goToPage}
-				username={contact}
-			/>
+			<div
+				className="bubble contact"
+				onClick={goToChat}>
+				{contact}
+			</div>
 		);
 	});
 
 	return (
 		<div className="page contacts">
 			{contacts}
-			<div className="bubble" onClick={goToPage}>add</div>
+			<div className="bubble" onClick={goToAddContact}>add</div>
 		</div>
 	)
 }
