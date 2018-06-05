3e0fd7a262bd8bf4436d26fdb8239111cffec0f8
Nuno Neves <youknownuno@gmail.com>
2018-06-05 23:41:40 +0100

    moved server directory to root

---
+++ server/socketAPI.js
@@ -0,0 +1,17 @@
+const openSocket = require('socket.io-client');
+const socket = openSocket('http://localhost:8000');
+
+const socketAPI = {
+	
+	// setup connect
+
+	// setup getHistory
+
+	// setup sendMessage
+
+	// setup sendWriting
+
+	// setup getWriting
+}
+
+module.exports = { socketAPI : socketAPI }; 
