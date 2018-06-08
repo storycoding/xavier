0b33f683dc6648f74610843a2d80253e47dcc33e
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-08 13:33:56 +0100

    integrated socketAPI.js with the login components,  refactored jsx, stubbed signup

--- src/Login.js
+++ src/Login.js
@@ -1,34 +1,74 @@
 import React, { Component } from 'react';
+import { socketAPI } from './socketAPI.js';
 
 class Login extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			username: "me",
+			email: "rob@gmail.com",
 			password: "password"
 		}
 	}
 
-	goToPage = () => {
-		this.props.goToPage("Contacts");
+	setCredentials = this.props.setCredentials;
+
+	goToContacts = () => { this.props.goToPage("Contacts") }
+
+	login = () => {
+
+		//make api call to socket
+		socketAPI.connect(this.state, () => {
+			socketAPI.getUserInfo(this.state, (response) => {
+				console.log("getUserInfo response from server: ", response);
+				const credentials = {
+					name: response.name,
+					email: response.email,
+					contacts: response.contacts
+				};
+				this.setCredentials(credentials);
+			});
+		});
+			// if credentials are correct
+				// go to contacts
+			// else
+				// detect where email or password were wrong
+				// ask for credentials again
+
+		
+		this.goToContacts();
 	}
 
-	submit = () => {
-		//make api call to socket
-			// use email and password
-		this.goToPage();
+	signUp = () => {
+		// trigger signUp page instead
+		// create SignUp.js or conditional render in login Component
 	}
-	
-	
+
+	handleInput = (event) => {
+		this.setState( {[event.target.name] : event.target.value} );
+	}
+
 
 	render() {
 		return (
-			<div className="page login">
-				<input type="text" placeholder="email" className="bubble"></input>
-				<input type="text" placeholder="password" className="bubble"></input>
-				<div className="bubble" onClick={this.submit}>login</div>
-			</div>
-			);
+			<form className="page login" onSubmit={this.login}>
+				<input
+					name="email"
+					type="text"
+					placeholder="example you@gmail.com"
+					className="bubble"
+					value={this.state.email}
+					onChange={this.handleInput}/>
+				<input
+					name="password"
+					type="text"
+					placeholder="your password"
+					className="bubble"
+					value={this.state.password}
+					onChange={this.handleInput}/>
+
+				<div className="bubble" onClick={this.login}>login</div>
+			</form>
+		);
 	}
 
 }
