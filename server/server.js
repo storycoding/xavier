ef6d7987373aef7af0c56d3da4500093f05de9c2
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-08 12:42:03 +0100

    refactored server auth to access user data in constant time lookup, refactored login verification to match the new format sent from the client websocket

--- server/server.js
+++ server/server.js
@@ -1,54 +1,89 @@
 const io = require('socket.io')();
-const socketAPI = require('./socketAPI.js').socketAPI;
+const socketAPI = require('../src/socketAPI.js').socketAPI;
 
 const port = process.env.port || 8000;
 
 let connections = 0;
 
+
 let fakeAuth = {
-	Rob : "password",
-	Laura : "password",
-	Distracted : "password",
-	Forgetful : "password",
+	'rob@gmail.com' : {
+		name: 'Rob',
+		email: 'rob@gmail.com',
+		password: 'password',
+		contacts: ["Laura"]
+	},
+
+	'laura@gmail.com' : {
+		name: 'Laura',
+		email: 'laura@gmail.com',
+		password: 'password',
+		contacts: ["Rob"]
+	},
+
+	'distracted@gmail.com' : {
+		name: 'Distracted',
+		email: 'distracted@gmail.com',
+		password: 'password',
+		contacts: []
+	},
+
+	'forgetful@gmail.com' : {
+		name: 'Forgetful',
+		email: 'forgetful@gmail.com',
+		password: 'password',
+		contacts: []
+	}
 };
 
 io.on('connection', (client) => {
 	let addedUser = false;
 
 	client.on('login', (credentials) => {
-		const { username, password } = credentials;
+		const { email, password } = credentials;
 
-		console.log(`${username} is trying to connect to the chat with password: ${password}`);
+		console.log(`${email} is trying to connect to the chat with password: ${password}`);
 
 		if (addedUser) {
 			client.emit('login response', 'already connected');
-			console.log(`${username} failed to connect`);
+			console.log(`${email} failed to connect`);
 			return;
 		}
 
 		else if (!password || password.length < 1) {
 			client.emit('login response', 'no password');
-			console.log(`${username} failed to connect`);
+			console.log(`${email} failed to connect`);
 			return;
 		}
 
 		// replace fakeAuth with call to database
-		else if( fakeAuth[username] !== password ) {
+		else if( fakeAuth[email].password !== password ) {
 			client.emit('login response', 'wrong password');
-			console.log(`${username} failed to connect`);
+			console.log(`${email} failed to connect`);
 			return;
 		}
 
 		else {
-			client.username = username;
+			client.email = email;
 			connections ++;
 			addedUser = true;
-			console.log(`${client.username} connected to the chat`);
+			console.log(`${client.email} connected to the chat`);
 			client.emit('login response', 'success');
 			return;
 		}
+	});
 
-		
+	client.on('getUserInfo', (credentials) => {
+		// check credentials
+			// send object
+				// name, email, contacts
+
+			// placeholder
+			client.emit('sendUserInfo', {
+				name : "Rob",
+				email : "rob@gmail.com",
+				contacts : ["Laura"]
+			});
 	});
 
 	// setup rooms
@@ -69,7 +104,7 @@ io.on('connection', (client) => {
 	// setup getWriting
 
 	client.on('disconnect', () => {
-		console.log(`${client.username} disconnected from the chat`);
+		console.log(`${client.email} disconnected from the chat`);
     if (addedUser) { connections -- }
   });
 
