fabf10ddee7605ac8d7b708451c4330aa8df1e81
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-08 23:16:21 +0100

    refactored schema to receiver proper authentication

--- database/schema.sql
+++ database/schema.sql
@@ -1,31 +1,34 @@
+DROP DATABASE xavier;
+
 CREATE DATABASE xavier;
 \c xavier;
 
-CREATE TABLE users (
-	user_id VARCHAR NOT NULL,
-	password VARCHAR NOT NULL,
-	PRIMARY KEY (user_id)
+CREATE TABLE accounts (
+	account_id SERIAL PRIMARY KEY,
+	email VARCHAR NOT NULL,
+	name VARCHAR NOT NULL,
+	salt VARCHAR NOT NULL,
+	password VARCHAR NOT NULL
 );
 
 CREATE TABLE connections (
-	user_id VARCHAR NOT NULL,
-	connection_id VARCHAR NOT NULL,
-	PRIMARY KEY (user_id)
+	a_id INT NOT NULL,
+	b_id INT NOT NULL,
+	PRIMARY KEY (a_id)
 );
 
 CREATE TABLE messages (
 	id SERIAL PRIMARY KEY,
+	publisher_id INT NOT NULL,
+	subscriber_id INT NOT NULL,
 	content VARCHAR NOT NULL,
-	publisher_id VARCHAR NOT NULL,
-	subscriber_id VARCHAR NOT NULL,
 	date_sent TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO users(user_id, password) VALUES ('Laura', 'password');
-INSERT INTO users(user_id, password) VALUES ('Rob', 'password');
+INSERT INTO accounts(email, name, salt, password) VALUES ('laura@hotmail.com','Laura','salt', 'password');
+INSERT INTO accounts(email, name, salt, password) VALUES ('rob@hotmail.com','Rob','salt', 'password');
 
-INSERT INTO connections(user_id, connection_id) VALUES ('Laura', 'Rob');
+INSERT INTO connections(a_id, b_id) VALUES ( (SELECT account_id FROM accounts WHERE name = 'Laura') , (SELECT account_id FROM accounts WHERE name = 'Rob') );
+INSERT INTO connections(a_id, b_id) VALUES ( (SELECT account_id FROM accounts WHERE name = 'Rob') , (SELECT account_id FROM accounts WHERE name = 'Laura') );
 
-INSERT INTO messages(content, publisher_id, subscriber_id) VALUES ('Hey dude, how you doin?', 'Laura', 'Rob');
-
-INSERT INTO messages(content, publisher_id, subscriber_id) VALUES ('Not much, are we hitting the pub tonight or what?', 'Rob', 'Laura');
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( (SELECT account_id FROM accounts WHERE name = 'Laura') , (SELECT account_id FROM accounts WHERE name = 'Rob') , 'Hey dude, how you doin?' );
