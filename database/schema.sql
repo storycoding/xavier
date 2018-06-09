0010ad4bbb0bfd50b66515058b50134f26857698
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-09 20:11:18 +0100

    changed primary key in connections, added dummy data

--- database/schema.sql
+++ database/schema.sql
@@ -11,9 +11,8 @@ CREATE TABLE accounts (
 );
 
 CREATE TABLE connections (
-	a_id INT NOT NULL,
-	b_id INT NOT NULL,
-	PRIMARY KEY (a_id)
+	a_id INT PRIMARY KEY,
+	b_id INT NOT NULL
 );
 
 CREATE TABLE messages (
@@ -30,4 +29,10 @@ INSERT INTO accounts(email, name, hash) VALUES ('rob@hotmail.com','Rob', 'hash')
 INSERT INTO connections(a_id, b_id) VALUES ( (SELECT account_id FROM accounts WHERE name = 'Laura') , (SELECT account_id FROM accounts WHERE name = 'Rob') );
 INSERT INTO connections(a_id, b_id) VALUES ( (SELECT account_id FROM accounts WHERE name = 'Rob') , (SELECT account_id FROM accounts WHERE name = 'Laura') );
 
-INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( (SELECT account_id FROM accounts WHERE name = 'Laura') , (SELECT account_id FROM accounts WHERE name = 'Rob') , 'Hey dude, how you doin?' );
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '1' , '2' , 'Hey dude, how you doin?' );
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '2' , '1' , 'Peachy. How are you Laura?' );
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '1' , '2' , 'Not gonna lie, this hayfever is not making it easy!' );
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '2' , '1' , 'I got just the thing for it.' );
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '1' , '2' , 'And what would that be?' );
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '2' , '1' , 'Lets go SURFING!' );
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '2' , '1' , ':D' );
