0e5aab2987494bf3cbc7d3cc5996783dcab399b0
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-09 11:55:30 +0100

    integrated chatBox with socketAPI to send data to the server

--- src/ChatBox.js
+++ src/ChatBox.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-
+import { socketAPI } from './socketAPI.js';
 class ChatBox extends Component {
-	constructor() {
-		super();
-		// controlled component
-		// holds state for API calls to the socket
+	constructor(props) {
+		super(props);
+		this.state = {
+			content:"",
+			publisher:props.publisher
+		}
 	}
 
-	//handleInput
+	handleInput = (event) => {
+		console.log("handleInput event.target.value: ", event.target.value);
+		this.setState( {content : event.target.value} );
+	}
 
-	//handleKeyPress
+	handleKeyPress = (event) => {
+		if(event.nativeEvent.keyCode == 13 && !event.nativeEvent.shiftKey) {
+			socketAPI.publish(this.state, (response) => {
+				console.log("response from server on publish: ", response);
+				// warning: the call back is being triggered cumulatively
 
-	//handleSubmit
+				this.setState( { content: "" } );
+			});	
+		}
+	}
 	
 	render() {
-		return <div className="chatBox">
-		<input type="textarea" className="textarea" placeholder="write something"></input>
-		</div>
+		return (
+			<textarea
+				className="chatBox"
+				type="content"
+				value={this.state.content}
+				onChange={this.handleInput}
+				onKeyPress={this.handleKeyPress}
+				placeholder="Write something here..."
+			/>
+		);
 	}
 
 }
