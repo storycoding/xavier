5c57f149f7618d06fd14211b20f22f28c6835841
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-10 22:42:57 +0100

    removed dummy data, added shouldComponentUpdate to optimize memory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-// import logo from './logo.svg'
 import './App.css'
 
 import Login from './Login.js'
@@ -7,6 +6,8 @@ import Loading from './Loading.js'
 import Contacts from './Contacts.js'
 import Chat from './Chat.js'
 import AddContact from './AddContact.js'
+
+import { socketAPI } from './socketAPI.js'
 
 class App extends Component {
   constructor() {
@@ -16,42 +17,39 @@ class App extends Component {
       page: 'Login',
 
       publisher: {
-        name: 'Rob',
-        id: 2
+        name: null,
+        id: null
       },
       
       subscriber: {
-        name: 'Laura',
-        id: 1
+        name: null,
+        id: null
       },
 
-      contacts: ['Laura'],
+      contacts: null,
 
-      history: [
-        { 
-          publisher: 2,
-          content: "How are you?"
-        },
-        {
-          publisher: 1,
-          content: "All good in the hood. You?"
-        },
-        {
-          publisher: 2,
-          content: "Peachy!"
-        }
-      ]
+      history: null
     }
+  }
+
+  goToPage = (page) => {
+    this.setState( { page : page } )
   }
 
   componentDidMount() {
       console.log('AppDidMount: ', this.state)
 
-      /*
-      socketAPI.getMessages(this.state.users, (response) => {
-        this.setState( { history: response } )
-      })
-      */
+      const users = {
+        publisher_id : this.state.publisher.id,
+        subscriber_id : this.state.subscriber.id
+      }
+
+      if(this.state.publisher.id) {
+        socketAPI.getMessages(users, (response) => {
+          this.setState( { history: response } )
+        })
+      }
+      
 
       // check for auth cookie
         // if cookie is valid
@@ -63,21 +61,33 @@ class App extends Component {
           // load Login page
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('AppShouldUpdate?')
+    if (this.state.history !== nextState.history) {
+      return true;
+    }
+
+    if (this.state.page !== nextState.page) {
+      return true;
+    }
+
+    return false;
+  }
+
   componentDidUpdate() {
       console.log('AppDidUpdate: ', this.state)
   }
 
+// get subscribe
   updateUserInfo = (info) => {
     console.log('App login info: ', info)
+
     this.setState( {
       page : 'Contacts',
       publisher : info.publisher,
+      subscriber : { name: 'Laura', id:'1'},
       contacts : info.contacts
      } )
-  }
-
-  goToPage = (page) => {
-    this.setState( { page : page } )
   }
 
   renderPage = (page) => {
