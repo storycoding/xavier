ab006b25d8a50e2b93928971f668fe86a98831a3
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-10 21:55:51 +0100

    added more accounts, and added the field b_name to connections to trade memory for querying speed

--- database/schema.sql
+++ database/schema.sql
@@ -4,15 +4,16 @@ CREATE DATABASE xavier;
 \c xavier;
 
 CREATE TABLE accounts (
-	account_id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	email VARCHAR UNIQUE NOT NULL,
 	name VARCHAR NOT NULL,
 	hash VARCHAR NOT NULL
 );
 
 CREATE TABLE connections (
-	a_id INT PRIMARY KEY,
-	b_id INT NOT NULL
+	a_id INT NOT NULL,
+	b_id INT NOT NULL,
+	b_name VARCHAR NOT NULL
 );
 
 CREATE TABLE messages (
@@ -25,9 +26,17 @@ CREATE TABLE messages (
 
 INSERT INTO accounts(email, name, hash) VALUES ('laura@hotmail.com','Laura', 'hash');
 INSERT INTO accounts(email, name, hash) VALUES ('rob@hotmail.com','Rob', 'hash');
+INSERT INTO accounts(email, name, hash) VALUES ('ollie@hotmail.com','Ollie', 'hash');
+INSERT INTO accounts(email, name, hash) VALUES ('susan@hotmail.com','Susan', 'hash');
 
-INSERT INTO connections(a_id, b_id) VALUES ( (SELECT account_id FROM accounts WHERE name = 'Laura') , (SELECT account_id FROM accounts WHERE name = 'Rob') );
-INSERT INTO connections(a_id, b_id) VALUES ( (SELECT account_id FROM accounts WHERE name = 'Rob') , (SELECT account_id FROM accounts WHERE name = 'Laura') );
+INSERT INTO connections(a_id, b_id, b_name) VALUES ( (SELECT id FROM accounts WHERE name = 'Laura') , (SELECT id FROM accounts WHERE name = 'Rob'), 'Rob' );
+INSERT INTO connections(a_id, b_id, b_name) VALUES ( (SELECT id FROM accounts WHERE name = 'Rob') , (SELECT id FROM accounts WHERE name = 'Laura'), 'Laura' );
+
+INSERT INTO connections(a_id, b_id, b_name) VALUES ( (SELECT id FROM accounts WHERE name = 'Ollie') , (SELECT id FROM accounts WHERE name = 'Rob'), 'Rob' );
+INSERT INTO connections(a_id, b_id, b_name) VALUES ( (SELECT id FROM accounts WHERE name = 'Rob') , (SELECT id FROM accounts WHERE name = 'Ollie'), 'Ollie' );
+
+INSERT INTO connections(a_id, b_id, b_name) VALUES ( (SELECT id FROM accounts WHERE name = 'Laura') , (SELECT id FROM accounts WHERE name = 'Susan'), 'Susan' );
+INSERT INTO connections(a_id, b_id, b_name) VALUES ( (SELECT id FROM accounts WHERE name = 'Susan') , (SELECT id FROM accounts WHERE name = 'Laura'), 'Laura' );
 
 INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '1' , '2' , 'Hey dude, how you doin?' );
 INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '2' , '1' , 'Peachy. How are you Laura?' );
@@ -35,4 +44,4 @@ INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '1' , '2' ,
 INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '2' , '1' , 'I got just the thing for it.' );
 INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '1' , '2' , 'And what would that be?' );
 INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '2' , '1' , 'Lets go SURFING!' );
-INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '2' , '1' , ':D' );
+INSERT INTO messages (publisher_id, subscriber_id, content) VALUES ( '1' , '2' , ':D' );
