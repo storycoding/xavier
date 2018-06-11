7275436a708e40b4f525d7e8a79df055eaffcdc8
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-11 23:40:32 +0100

    refactored to match props

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
 const Contacts = (props) => {
+	const goToAddContact = () => props.updatePage('AddContact')
 
-	const goToAddContact = () => { props.goToPage('AddContact') }
+	const handleClick = (contact) => {
+		props.updateSubscriber(contact)
+		props.updatePage('Chat')
+	}
 
-	const goToChat = () => props.goToPage('Chat')
-
-	// clicking a contact should update the App.state.subscriber
 	const contacts = props.contacts.map( (contact, index) => {
 		return (
 			<div
 				key={index}
+				value={contact}
 				className='bubble contact'
-				onClick={goToChat}>
-				{contact}
+				onClick={handleClick.bind(null, contact)}>
+				{contact.name}
 			</div>
 		)
 	})
