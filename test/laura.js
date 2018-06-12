26bddea1fcd700d45da2cce9b6f0025f8c7dce60
storycoding <30328808+storycoding@users.noreply.github.com>
2018-06-12 15:15:38 +0100

    fakeData.js moved to another file, userTests combined into multipleLogin.js

--- test/laura.js
+++
@@ -1,8 +0,0 @@
-const { socketAPI } = require('../server/socketAPI.js');
-
-socketAPI.connect({
-	username : "Laura",
-	password : "password"
-}, console.log);
-
-socketAPI.send({publisher: "Laura", content: "Hi Rob!"}, console.log);
--- test/rob.js
+++
@@ -1,8 +0,0 @@
-const { socketAPI } = require('../server/socketAPI.js');
-
-socketAPI.connect({
-	username : "Rob",
-	password : "password"
-}, console.log);
-
-socketAPI.send({publisher: "Rob", content: "Hello!"}, console.log);
